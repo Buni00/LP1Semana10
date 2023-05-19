58d12fc4a592682fe3d60737a4ebae9e0c0c3fe4
Bruno Fernandes <a22103346@alunos.ulht.pt>
2023-05-19 13:48:26 +0100

    Change namespace

---
+++ PlayerManager4/Player.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+
+namespace PlayerManager4
+{
+    public class Player : IComparable<Player>
+    {
+        public string Name { get; }
+        public int Score { get; }
+
+        public Player(string name, int score)
+        {
+            Name = name;
+            Score = score;
+        }
+
+        public int CompareTo(Player other)
+        {
+            return Score.CompareTo(other.Score);
+        }
+    }
+}
